11a01b9a52842b8415c0ad10d8bba9d0f77db69f
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-04-29 13:34:40 -0400

    Create login_stuff.php

---
+++ login_stuff.php
@@ -0,0 +1,29 @@
+<?PHP
+session_start();
+require("functions.php");
+?>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <header>
+            <h0>
+                Login
+            </h0>
+        </header>
+        <form>
+            <fieldset>
+                <label>
+                    Email
+                </label>
+                <input type="
+                
+                
+                
+                <input type="submit" id="submit" value="Login"/>
+            </fieldset>
+        </form>
+    </body>
+
+</html>
