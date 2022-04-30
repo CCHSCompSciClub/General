472f276b343930abe44a996c38714ae23e3b166f
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-04-30 16:47:34 -0400

    Create main.php

---
+++ main.php
@@ -0,0 +1,28 @@
+<?PHP
+session_start();require('functions.php');
+?>
+<html>
+  <head>
+  
+  
+  
+  </head>
+  <body>
+    <header>
+      <h1>
+        Main Page
+      </h1>
+    </header>
+    <p>
+    This is the main page!
+    
+    </p>
+  
+  
+  
+  
+  
+  
+  </body>
+
+</html>
