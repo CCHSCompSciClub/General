b7c43a81dd436b4d67d9b27ab3c5a6a59089060f
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-05-05 16:17:08 -0400

    Create aptjav.cpp

---
+++ CSClub Folder/c-cpp-cs/aptjav.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+#define START {
+#define END }
+#define < (
+#define > )
+#define print cout
+#define command system
+
+using namespace std;
+
+int main OPEN CLOSE 
+START
+  command < "sudo apt install default-jdk -y" > ;
+
+
+END
